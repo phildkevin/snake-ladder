a36552fe1020d931c7bd596b5c79cfc4a36c5cef
phil kevin <phildkevin24@gmail.com>
2019-04-16 02:33:03 +0800

    changes

--- public/assets/js/game/multiplayer.js
+++ public/assets/js/game/multiplayer.js
@@ -10,10 +10,28 @@ let matchMaking = () =>{
 
 let plotGamePlayerList = (players) =>{
   if(players.length > 0){
+    var player = ``;
     for(var x = 0; x < players.length; x++){
       let you   = localStorage.getItem("name");
-      let ifyou = you == players[x].name ? "(You)" : ""
-      $('#player_list').append(`<h5>${players[x].name} ${ifyou}</h5>`)
+      let ifyou = you == players[x].name ? "(You)" : players[x].name;
+
+      console.log(players[x].name);
+      if(players[x].name == you){
+        player = `
+          <div class="other-player active">
+            <span class="other-player-info">${ifyou}<b class="other-player-stat">(title)</b></span>
+          </div>
+        `;
+      }else{
+        player = `
+          <div class="other-player">
+            <span class="other-player-info">${players[x].name}<b class="other-player-stat">(title)</b></span>
+          </div>
+        `;
+      }
+
+      $('#other-player-list').append(player);
+
     }
   }
 }
